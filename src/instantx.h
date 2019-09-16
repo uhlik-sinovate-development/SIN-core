1fdf51dd49efa2cdb0038cbdcb20f171a11de892
Giacomo Milligan <giaki3003@gmail.com>
2019-09-16 17:53:10 +0200

    [quorum-tx] Refine locking logic and make sure we only check locks held
    pwallet now is passed on as arg, and we let locks fail (and return if so). Fixes most segfaults on my side

--- src/instantx.h
+++ src/instantx.h
@@ -9,6 +9,7 @@
 
 #include <chain.h>
 #include <net.h>
+#include <wallet/wallet.h>
 #include <primitives/transaction.h>
 
 class CTxLockVote;
@@ -70,7 +71,7 @@ private:
     void Vote(CTxLockCandidate& txLockCandidate, CConnman& connman);
 
     //process consensus vote message
-    bool ProcessTxLockVote(CNode* pfrom, CTxLockVote& vote, CConnman& connman);
+    bool ProcessTxLockVote(CNode* pfrom, CTxLockVote& vote, CConnman& connman, CWallet* pwallet);
     void ProcessOrphanTxLockVotes(CConnman& connman);
     bool IsEnoughOrphanVotesForTx(const CTxLockRequest& txLockRequest);
     bool IsEnoughOrphanVotesForTxAndOutPoint(const uint256& txHash, const COutPoint& outpoint);
